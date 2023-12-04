4cba9fd1dd8bf36d068452e2c0e4a3b92e9ba9e1
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-12-04 14:40:24 +0200

    Tests getUserId by function

--- tests/user.test.js
+++ tests/user.test.js
@@ -4,6 +4,7 @@ const listen = require('test-listen');
 const got = require('got');
 
 const app = require('../index.js')
+const { userUserIdGET } = require('../service/UserService.js')
 
 test.before(async (t) => {
     t.context.server = http.createServer(app);
@@ -13,6 +14,34 @@ test.before(async (t) => {
 
 test.after.always((t) => {
     t.context.server.close();
+})
+
+test('GET user by function', async (t) => {
+    const res  = await userUserIdGET(1)
+
+    // expected keys response should have
+    const expectedKeys = {
+        password: 'string',
+        teamdId: 'number',
+        isAdmin: 'boolean',
+        userId: 'number',
+        BasicInformation: 'object',
+    }
+
+    // check if response is truthy
+    t.assert(res)
+
+    // check if all the expected keys are in the response object
+    for (let key of Object.keys(expectedKeys))
+        t.true(key in res)
+
+    // check if values are the expected type
+    for (let [key, type] of Object.entries(expectedKeys))
+        t.is(typeof res[key], type)
+
+    // check if the retrieved user is the expected user
+    t.is(res.isAdmin, true)
+    t.is(res.userId, 1)
 })
 
 test('GET user', async (t) => {
