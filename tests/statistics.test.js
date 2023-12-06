5c819a1e7227e454cd7b229befd9deb662b0867d
AnastasisBeltes <anastasisbeltes@gmail.com>
2023-12-06 16:08:19 +0200

    Change file on Test Get Team Contacts

--- tests/statistics.test.js
+++ tests/statistics.test.js
@@ -154,3 +154,33 @@ test('PUT Statistics - Bad Request', async (t) =>{
     t.is(error.response.statusCode, 400);
     t.is(error.message, 'Response code 400 (Bad Request)');
 });
+
+
+test('Get team contacts', async(t) =>{
+    const { body , statusCode }=await t.context.got('user/1/team/2/contacts')
+    // check status code
+    t.is(statusCode,200)
+
+    // getting the first contract
+    const firstelemnt = body[0]
+
+    // check the info 
+    t.is(firstelemnt.name,'tasos')
+    t.is(firstelemnt.surname,'karakoul')
+    t.is(firstelemnt.profileimage,'1111111')
+
+    // getting the second contract
+    const second = body[1]
+
+    // check the info 
+    t.is(second.name,'giwrgos')
+    t.is(second.surname,'gkyzis')
+    t.is(second.profileimage,'0000001')
+
+    // check the type 
+    const Keys= ['name', 'surname','profileimage']
+    Keys.forEach((x)=>{t.true(firstelemnt.hasOwnProperty(x))})
+
+
+})
+
--- tests/user.test.js
+++ tests/user.test.js
@@ -81,31 +81,3 @@ test('GET user 400', async (t) => {
     const error = await t.throwsAsync(async () => await t.context.got('user/asdas'), {instanceOf: got.HTTPError})
     t.is(error.message, 'Response code 400 (Bad Request)')
 })
-
-test('Get team contacts', async(t) =>{
-    const { body , statusCode }=await t.context.got('user/1/team/2/contacts')
-    // check status code
-    t.is(statusCode,200)
-
-    // getting the first contract
-    const firstelemnt = body[0]
-
-    // check the info 
-    t.is(firstelemnt.name,'tasos')
-    t.is(firstelemnt.surname,'karakoul')
-    t.is(firstelemnt.profileimage,'1111111')
-
-    // getting the second contract
-    const second = body[1]
-
-    // check the info 
-    t.is(second.name,'giwrgos')
-    t.is(second.surname,'gkyzis')
-    t.is(second.profileimage,'0000001')
-
-    // check the type 
-    const Keys= ['name', 'surname','profileimage']
-    Keys.forEach((x)=>{t.true(firstelemnt.hasOwnProperty(x))})
-
-
-})
