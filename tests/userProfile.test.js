e2b1ef1c4e7fe361c7aef6a6c2a9e5c39e95f43f
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-12-11 14:47:14 +0200

    Refactors user testing into two files

--- tests/userProfile.test.js
+++ tests/userProfile.test.js
@@ -1,42 +1,38 @@
-const http = require('http');
 const test = require('ava');
-const listen = require('test-listen');
 const got = require('got');
 
-const app = require('../index.js');
+const { setupServer } = require('../utils/testServer.js')
 const { userUserIdGET } = require('../service/UserService.js')
 
 test.before(async (t) => {
-    t.context.server = http.createServer(app);
-    t.context.prefixUrl = await listen(t.context.server);
-    t.context.got = got.extend({ prefixUrl: t.context.prefixUrl, responseType: 'json' });
+    t.context = await setupServer()
 });
 
 test.after.always((t) => {
     t.context.server.close();
 })
 
+// keys of user object
+const userKeys = {
+    password: 'string',
+    teamdId: 'number',
+    isAdmin: 'boolean',
+    userId: 'number',
+    BasicInformation: 'object',
+}
+
 test('GET user by function', async (t) => {
     const res  = await userUserIdGET(1)
-
-    // expected keys response should have
-    const expectedKeys = {
-        password: 'string',
-        teamdId: 'number',
-        isAdmin: 'boolean',
-        userId: 'number',
-        BasicInformation: 'object',
-    }
 
     // check if response is truthy
     t.assert(res)
 
     // check if all the expected keys are in the response object
-    for (let key of Object.keys(expectedKeys))
+    for (let key of Object.keys(userKeys))
         t.true(key in res)
 
     // check if values are the expected type
-    for (let [key, type] of Object.entries(expectedKeys))
+    for (let [key, type] of Object.entries(userKeys))
         t.is(typeof res[key], type)
 
     // check if the retrieved user is the expected user
@@ -47,24 +43,15 @@ test('GET user by function', async (t) => {
 test('GET user 200', async (t) => {
     const { body, statusCode} = await t.context.got('user/1');
 
-    // expected keys response should have
-    const expectedKeys = {
-        password: 'string',
-        teamdId: 'number',
-        isAdmin: 'boolean',
-        userId: 'number',
-        BasicInformation: 'object',
-    }
-
     // check if response is truthy
     t.assert(body)
 
     // check if all the expected keys are in the response object
-    for (let key of Object.keys(expectedKeys))
+    for (let key of Object.keys(userKeys))
         t.true(key in body)
 
     // check if values are the expected type
-    for (let [key, type] of Object.entries(expectedKeys))
+    for (let [key, type] of Object.entries(userKeys))
         t.is(typeof body[key], type)
 
     // check if the retrieved user is the expected user
@@ -145,60 +132,6 @@ test("Put user 400" ,async (t)=>{
     });
 
     // Access the properties of the caught error
-    t.is(error.response.statusCode, 400);
-    t.is(error.message, 'Response code 400 (Bad Request)');
-});
-
-test('PUT user login 200', async (t) => {
-
-    // a body for Put request 
-    const requestBody={
-        "email" : "Testing@mpeltes.gr",
-        "password" : "testpassword"
-    }
-
-    // expected key response should have
-    const expectedKey={
-        "token" : 'string'
-    }
-
-    const {body , statusCode} = await t.context.got.put("user/login", {
-        json: requestBody,
-        responseType: 'json', 
-    })
-
-    //check if response is truthy
-    t.assert(body)
-    //check statuscode==200
-    t.is(statusCode,200)
-    //check the values 
-    t.is(body.token,'000001')
-
-    // check if all the expected keys are in the response object
-    for (let key of Object.keys(expectedKey))
-        t.true(key in body)
-
-    // check if values are the expected type
-    for (let [key, type] of Object.entries(expectedKey))
-        t.is(typeof body[key], type)
-});
-
-test('PUT user login 400', async (t) => {
-    // a body for Put request 
-    const requestBody={
-        "email" : "Testing@mpeltes.gr",
-        "password" : "testpassword"
-    }
-
-    // check bad request through bad request
-    const error = await t.throwsAsync(async () => {
-        const res = await t.context.got.put('user/loginERROR',{
-            json: requestBody,
-            responseType: 'json', 
-          });
-    });
-    
-    // check the error response
     t.is(error.response.statusCode, 400);
     t.is(error.message, 'Response code 400 (Bad Request)');
 });
