47b43b3325137f33f560655c67fb93ae17b76786
George Gkyzis <gkyzisgeorge@gmail.com>
2023-12-19 11:35:52 +0200

    writeJson function test with default code(200)

--- tests/writer.test.js
+++ tests/writer.test.js
@@ -16,3 +16,14 @@ const createMockResponse = () => {
 };
 
 // Testing differenct cases related to write.js file that handles the APIs responses
+
+// writeJson fuction default code
+test('writeJson function - default code (200)', (t) => {
+    const response = createMockResponse();
+    writeJson(response, { message: 'Success' });
+
+    const responseData = JSON.parse(response.payload);
+    t.is(response.statusCode, 200);
+    t.is(responseData.message, 'Success');
+    t.is(response.headers['Content-Type'], 'application/json');
+});
