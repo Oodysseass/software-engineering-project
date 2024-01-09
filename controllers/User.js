dda14ffb6194c57fe9daeac390c377ce68c9236d
George Gkyzis <gkyzisgeorge@gmail.com>
2024-01-10 01:33:22 +0200

    Detailed function comments on createTeam, getContacts and getStatistics functions.

--- controllers/User.js
+++ controllers/User.js
@@ -5,12 +5,20 @@ var utils = require('../utils/writer.js');
 // Importing the UserService module for handling user-related operations
 var User = require('../service/UserService');
 
-// Endpoint for creating a new team
+/*
+  Endpoint for creating a new team
+
+  body: the request body holding an object with the name of the new team
+
+  userId: integer specifying the id of the user that requests to create the team
+
+  returns http response based on the result of the operation
+*/
 module.exports.createTeam = function createTeam (req, res, next, body, userId) {
   // Call the createTeam function from the UserService, passing the provided body and userId
   User.createTeam(body, userId)
     .then(function (response) {
-      // Write the JSON response with the result of the operation
+      // Write the JSON response with the result of the operation - Team Successfully Created
       utils.writeJson(res, response);
     })
     .catch(function (response) {
@@ -19,12 +27,20 @@ module.exports.createTeam = function createTeam (req, res, next, body, userId) {
     });
 };
 
-// Endpoint for retrieving contacts for a user within a specific team
+/*
+  Endpoint for retrieving contacts for a user within a specific team
+
+  userId: integer specifying the id of the user that requests to access the team contacts
+
+  teamId: integer specifying the id of the team of the user
+
+  returns http response based on the result of the operation
+*/
 module.exports.getContacts = function getContacts (req, res, next, userId, teamId) {
   // Call the getContacts function from the UserService, passing the userId and teamId
   User.getContacts(userId, teamId)
     .then(function (response) {
-      // Write the JSON response with the retrieved contacts
+      // Write the JSON response with the retrieved contacts - Successful Operation
       utils.writeJson(res, response);
     })
     .catch(function (response) {
@@ -33,12 +49,20 @@ module.exports.getContacts = function getContacts (req, res, next, userId, teamI
     });
 };
 
-// Endpoint for retrieving statistics for a user within a specific team
+/*
+  Endpoint for retrieving statistics for a user within a specific team
+
+  userId: integer specifying the id of the user that requests to access the team statictics
+
+  teamId: integer specifying the id of the team of the user
+
+  returns http response based on the result of the operation
+*/
 module.exports.getStatistics = function getStatistics (req, res, next, userId, teamId) {
   // Call the getStatistics function from the UserService, passing the userId and teamId
   User.getStatistics(userId, teamId)
     .then(function (response) {
-      // Write the JSON response with the retrieved statistics
+      // Write the JSON response with the retrieved statistics - Successful Operation
       utils.writeJson(res, response);
     })
     .catch(function (response) {
